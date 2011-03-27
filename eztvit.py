5d7d6e5ac1b4dee0f4273bd55c00fcefa7acb39f
Anton P. Linevich <anton@linevich.com>
2011-03-28 02:35:48 +0300

    some raw version of eztvit watcher added

--- eztvit.py
+++ eztvit.py
@@ -2,16 +2,55 @@
 # encdoing: utf-8
 import sys
 import urllib2
+import re
 
 import lxml.html
 
-def showid_by_name():
+
+PAT_EPISODEINFO = [re.compile(ur"^(.+)[\t\s]*s(\d\d)e(\d\d)"),
+                   re.compile(ur"^(.+)[\s\t](\d+)x(\d+)"),
+                   ]
+
+global SHOWS_MAP_CACHED
+SHOWS_MAP_CACHED = []
+
+def episodeinfo_from_filename(filename):
+    filename = filename.lower()
+    for episodeinfo in PAT_EPISODEINFO:
+        match = episodeinfo.search(filename)
+        if match:
+            result = match.groups()
+            return result[0].strip(), int(result[1]), int(result[2])
+    return None
+
+
+def showid_by_name(name=None):
+    """
+    Returns eztv show id if there is a 100% match by show title
+    OR throw ValueError
+    """
+    result = filter(lambda kv: name.lower() == kv[1].lower(), shows_map())
+    if result and len(result) == 1:
+        return result[0][0]
+    else:
+        raise ValueError(u"TVShow '%s' not found" % name)
+
+
+def shows_map(nocache=False):
     """
     Returns mapping of showID nad showName
     """
-    tree = lxml.html.parse('http://eztv.it/')
-    for _el in tree.xpath('//select[@name="SearchString"]/option'):
-        yield(_el.get('value'), _el.text)
+    global SHOWS_MAP_CACHED
+    if not SHOWS_MAP_CACHED or nocache:
+        result = []
+        tree = lxml.html.parse('http://eztv.it/')
+        for _el in tree.xpath('//select[@name="SearchString"]/option'):
+            if _el.get('value'):
+                result.append((_el.get('value'), _el.text))
+        SHOWS_MAP_CACHED = result
+    return SHOWS_MAP_CACHED
+
+
 
 def torrents(showid):
     """
@@ -22,4 +61,5 @@ def torrents(showid):
     for _tr in tree.xpath('//table[@class="forum_header_noborder"]/tr[@class="forum_header_border"]'):
         yield {'filename': _tr.xpath('td[2]/a/text()')[0],
                'torrents': _tr.xpath('td[3]/a/@href')}
+
 
--- eztvit_test.py
+++ eztvit_test.py
@@ -4,5 +4,6 @@ print list(eztvit.showid_by_name())
 
 print list(eztvit.torrents('405'))
 
+print eztvit.showid_by_name("Bobs Burgers")
 
 
